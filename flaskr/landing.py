d52f27caf923fbc9dac27882fb2c85ec1647ed2c
xu yangbo <xu_yangbo@tech.gov.sg>
2022-03-14 14:58:45 +0800

    initial commit

---
+++ flaskr/landing.py
@@ -0,0 +1,22 @@
+import functools
+from random import randrange
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from flaskr.db import get_db
+
+bp = Blueprint('landing', __name__)
+
+@bp.route('/', methods=('GET',))
+def index():
+    msgs = [
+        "Logic will get you from A to B. Imagination will take you everywhere.",
+        "There are 10 inds of people. Those who know binary and those who dont.",
+        "There are two ways of constructing a software design. One way is to make it so simple that there are obvioursly no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.",
+        "It's not that I'm so smart, it's just that I stay with problems longer.",
+        "It is pitch dark. You are lilely to be eaten by a grue."
+    ]
+    msg = msgs[randrange(len(msgs))]
+    return render_template('landing/index.html', msg=msg)
