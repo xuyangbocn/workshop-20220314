98e55f0683756234b11ca48b115367bb02b84bbe
xu yangbo <xu_yangbo@tech.gov.sg>
2022-04-05 14:41:03 +0800

    commit for workshops during training

--- pre-workshop assignment/flaskr/landing.py
+++ pre-workshop assignment/flaskr/landing.py
@@ -13,10 +13,10 @@ bp = Blueprint('landing', __name__)
 def index():
     msgs = [
         "Logic will get you from A to B. Imagination will take you everywhere.",
-        "There are 10 inds of people. Those who know binary and those who dont.",
+        "There are 10 kinds of people. Those who know binary and those who dont.",
         "There are two ways of constructing a software design. One way is to make it so simple that there are obvioursly no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.",
         "It's not that I'm so smart, it's just that I stay with problems longer.",
-        "It is pitch dark. You are lilely to be eaten by a grue."
+        "It is pitch dark. You are likely to be eaten by a grue."
     ]
     msg = msgs[randrange(len(msgs))]
     return render_template('landing/index.html', msg=msg)
